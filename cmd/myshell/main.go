e57d1e02f0fb3b81e296627ff6c442542302b522
Lexon2 <lexonsss2@gmail.com>
2025-01-14 10:13:08 +0200

    feat: Implement RELP with stop command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,15 +10,25 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		// len(command)-1 removes the newline character
+
+		exactCommand := command[:len(command)-1]
+
+		switch exactCommand {
+		case "stop":
+			os.Exit(0)
+		default:
+			fmt.Println(exactCommand + ": command not found")
+		}
 	}
-	// len(command)-1 removes the newline character
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
